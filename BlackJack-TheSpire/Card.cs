b61886525294a7a84ac23dfa1a8c9bcd7398ea69
poui0609 <r3657@naver.com>
2026-05-13 01:24:08 +0900

    asd

---
+++ BlackJack-TheSpire/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack_TheSpire
+{
+    enum CardType
+    {
+        Spade, Club, Diamond, Heart
+    }
+
+    enum CardValue
+    {
+        Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+
+    internal class Card
+    {
+
+    }
+}
