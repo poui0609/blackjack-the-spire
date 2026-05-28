500ecfc60b197db6f9a516fa56381decbfd1a05b
동희 김 <kimdonghee0514@gmail.com>
2026-05-28 15:50:55 +0900

    .

--- BlackJack-TheSpire/Form1.cs
+++ BlackJack-TheSpire/Form1.cs
@@ -392,22 +392,6 @@ namespace BlackJack_TheSpire
 
 
         }
-        private void 게임종료ToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // 확인용 메시지 박스
-            DialogResult result = MessageBox.Show(
-                "정말로 게임을 종료하시겠습니까?",
-                "게임 종료",
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Question
-            );
-
-            if (result == DialogResult.Yes)
-            {
-                Application.Exit(); // 프로그램 종료
-            }
-            // No 선택하면 아무 일도 없음
-        }
         private void 나가기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             gamestarting();
@@ -467,6 +451,22 @@ namespace BlackJack_TheSpire
             RefreshInventory();
             MessageBox.Show(target.Name + " 아이템 삭제 완료!");
         }
+
+        private void 게임종료ToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            // 확인용 메시지 박스
+            DialogResult result = MessageBox.Show(
+                "정말로 게임을 종료하시겠습니까?",
+                "게임 종료",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit(); // 프로그램 종료
+            }
+            // No 선택하면 아무 일도 없음
+        }
     }
-    //gg
 }
